610ba10de9dbe02b2f4724e89583dac7f53ef12c
Eli Goberdon <goberdon@gmail.com>
2015-10-04 17:46:09 -0400

    rudimentary functionality, takes a process and repeats it if exit is entered, exits

--- main.c
+++ main.c
@@ -12,7 +12,25 @@
 #include <signal.h>
 
 int main(int argc, char **argv) {
-
+    int running = 1;
+    char * prompt = "Enter a command>>";
+    while (running){
+      printf("%s", prompt);
+      fflush(stdout);
+      char buffer[1024];
+      while (fgets(buffer, 1024, stdin) != NULL) {
+          int len = strlen(buffer);
+          if (buffer[len-1] == '\n'){
+            buffer[len-1] = '\0';
+          }
+          if (strncmp(buffer, "exit", len) == 0){
+            exit(0);
+          }
+          else{
+            printf("%s\n", buffer);
+          }
+          break;
+        }
+    }
     return 0;
 }
-
