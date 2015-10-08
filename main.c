3219b55c82648a50bb8abebd639773041cb55c52
Eli Goberdon <goberdon@gmail.com>
2015-10-07 21:50:03 -0400

    semi parser works for under 3 commands

--- main.c
+++ main.c
@@ -38,20 +38,20 @@ char** tokenify(const char *s) {
       s4[i] = strdup(strtok(NULL, badstuff));
       }
     }
-    s4[num_of_words] = NULL;
     free(s3);
+    s4[num_of_words] = NULL;
     return s4;
 }
 
-int *num_tokens(char *tokens[]) {
-    int *num = malloc(sizeof(int));
-    int i = 0;
-    while (tokens[i] != NULL) {
-        i++;
-    }
-    *num = i;
-    return num;
-}
+// int *num_tokens(char *tokens[]) {
+//     int *num = malloc(sizeof(int));
+//     int i = 0;
+//     while (tokens[i] != NULL) {
+//         i++;
+//     }
+//     *num = i;
+//     return num;
+// }
 
 void print_tokens(char *tokens[]) {
     int i = 0;
@@ -82,31 +82,39 @@ void free_tokens(char **tokens) {
     free(tokens); // then free the array
 }
 
-void free_commands(char *** runs){
-  int j = 0;
-  while (runs[j] != NULL){
-    int k = 0;
-    while(runs[j][k]!=NULL){
-      free(runs[j][k]);
+char *** semi_parser(char **command_list){
+  //int * num = num_tokens(command_list);
+  //int n = *num;
+  int i = 0;
+  int j = 0; //number of seperate commands
+  int k = 0; //number of words per command
+  int num_per_command[300];//use this to store number of words per [j]
+  while (command_list[i] != NULL){//Count number of commands, count number of words in each command
+    char * word = command_list[i];
+    int len = strlen(word);
+    if(strncmp(word,";",len)==0){
+      num_per_command[j] = k;
+      k = 0;
+      j++;
+    }
+    else{
       k++;
     }
-    free(runs[j]);
+    i++;
+  }
+  if(strncmp(command_list[i-1],";",strlen(command_list[i-1]))!=0){ //if the last string was not a semicolon, one more store needs to happen
+    num_per_command[j] = k;
     j++;
   }
-  free(runs);
-}
-
-char *** semi_parser(char **command_list){
-  int * num = num_tokens(command_list); //bigger than we need, but simplifies/speeds up the semi_parser
-  int n = *num;
-  char ***master_list = malloc(sizeof(char **) * (n+1));
-  for(int x = 0; x < n+1; x++){
-    char **inner = malloc(sizeof(char *) * n);
-    master_list[x] = inner;
+  char ***master_list = malloc(sizeof(char **) * j);
+  master_list[j] = NULL;
+  for(int x = 0; x < j; x++){
+    master_list[x] = malloc(sizeof(char *) * num_per_command[x]);
   }
-  int i = 0;
-  int j = 0;
-  int k = 0;
+
+  i = 0;
+  j = 0;
+  k = 0;
   while (command_list[i] != NULL){
     char * word = command_list[i];
     int len = strlen(word);
@@ -121,8 +129,10 @@ char *** semi_parser(char **command_list){
     }
     i++;
   }
-  master_list[j+1] = NULL;
-  free(num);
+  if(strncmp(command_list[i-1],";",strlen(command_list[i-1]))!=0){ //if the last string was not a semicolon, one more store needs to happen
+    master_list[j][k] = NULL;
+  }
+  //free(num);
   return master_list;
 }
 
@@ -140,12 +150,18 @@ void run_command(char * arg, char ** parameters){
 
 int main(int argc, char **argv) {
     int running = 1;
-    char * prompt = "Enter a command>>";
     while (running){
-      printf("%s", prompt);
+      printf("Enter a command>>");
       fflush(stdout);
       char buffer[1024];
+      if (running!=1){
+        break;
+      }
       while (fgets(buffer, 1024, stdin) != NULL) {
+          if (feof(stdin)){
+            running = 0;
+            break;
+          }
           if (buffer[0] =='\n'){
             break;
           }
@@ -155,24 +171,29 @@ int main(int argc, char **argv) {
             buffer[len-1] = '\0';
           }
           if (strncmp(buffer, "exit", len) == 0){ //sometimes have to type this twice? after other commands have been typed
-            exit(0);
+            running = 0;
+            break;
           }
           else if (strncmp(buffer, "clear", len) == 0){
-            system("clear");
+            system("clear"); //I like a clean terminal
           }
           else if (strncmp(buffer, "mode", len) == 0){
             printf("Mode not yet implemented\n");
           }
           else{
             char ** word_list = tokenify(buffer);
+            print_tokens(word_list);
             char *** runs = semi_parser(word_list);
+            printf("Going to free tokens now\n");
+            free_tokens(word_list);
+            print_parser(runs);
             int j = 0;
             while(runs[j] != NULL){
               run_command(runs[j][0], runs[j]);
+              free_tokens(runs[j]);
               j++;
             }
-            free_tokens(word_list);
-            free_commands(runs);
+            free(runs);
           }
           break;
         }
